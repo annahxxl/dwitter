812c5aaeef648dd33a80080509d02ad14bcc5a72
Hanna Lee <8annahxxl@gmail.com>
2022-01-13 21:21:05 +0900

    feat: Tweet CRUD APIs 기능 구현

--- server/src/router/tweets.js
+++ server/src/router/tweets.js
@@ -1,6 +1,73 @@
 import express from "express";
 import "express-async-errors";
 
+let tweets = [
+  {
+    id: "2",
+    text: "두번째 글!",
+    createdAt: Date.now().toString(),
+    name: "Hanna",
+    username: "hanna",
+  },
+  {
+    id: "1",
+    text: "첫번째 글!",
+    createdAt: Date.now().toString(),
+    name: "Bob",
+    username: "bob",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+];
+
 const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((tweet) => tweet.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
+router.post("/", (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets.unshift(tweet);
+  res.status(201).json(tweet);
+});
+
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id${id} not found` });
+  }
+});
+
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
 
 export default router;
