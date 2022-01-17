05ad0333a0cf391687d539c247f315d7ba2f79c1
Hanna Lee <8annahxxl@gmail.com>
2022-01-17 20:47:56 +0900

    feat: 유효성 검사 적용

--- server/src/routers/tweets.js
+++ server/src/routers/tweets.js
@@ -1,16 +1,26 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
+import { validate } from "../middlewares/validator.js";
 import * as tweetController from "../controllers/tweets.js";
 
 const router = express.Router();
+
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 characters"),
+  validate,
+];
 
 router.get("/", tweetController.getTweets);
 
 router.get("/:id", tweetController.getTweet);
 
-router.post("/", tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 
 router.delete("/:id", tweetController.deleteTweet);
 
