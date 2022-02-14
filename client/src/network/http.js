0b59dd45eff4ed138671b2d58c5a952a2ead31ef
Hanna Lee <8annahxxl@gmail.com>
2022-02-15 06:05:08 +0900

    feat: Axios 사용하기

--- client/src/network/http.js
+++ client/src/network/http.js
@@ -1,38 +1,39 @@
+import axios from "axios";
+
 export default class HttpClient {
   constructor(baseURL, authErrorEventBus, getCsrfToken) {
-    this.baseURL = baseURL;
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
+    this.client = axios.create({
+      baseURL,
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    });
   }
 
   async fetch(url, options) {
-    const res = await fetch(`${this.baseURL}${url}`, {
-      ...options,
+    const { body, method, headers } = options;
+    const req = {
+      url,
+      method,
       headers: {
-        "Content-Type": "application/json",
-        ...options.headers,
+        ...headers,
         "dwitter-csrf-token": this.getCsrfToken(),
       },
-      credentials: "include",
-    });
-    let data;
+      data: body,
+    };
+
     try {
-      data = await res.json();
+      const res = await this.client(req);
+      return res.data;
     } catch (error) {
-      console.error(error);
-    }
-
-    if (res.status > 299 || res.status < 200) {
-      const message =
-        data && data.message ? data.message : "Something went wrong.";
-      const error = new Error(message);
-      if (res.status === 401) {
-        this.authErrorEventBus.notify(error);
-        return;
+      if (error.response) {
+        const data = error.response.data;
+        const message =
+          data && data.message ? data.message : "Something went wrong";
+        throw new Error(message);
       }
-      throw error;
+      throw new Error("connection error");
     }
-
-    return data;
   }
 }
